9bd42a58dfe04176385bcfbd9540effc99cb1909
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-02-06 13:28:30 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    <button className="btn">Hello daisyUI</button>
-  </div>
+    <><div>
+      <button className="btn">Hello daisyUI</button>
+    </div><div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div></>
+  
   );
 }
 
