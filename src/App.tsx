dc462d9e309df5795c499a52b7de49bececd62ef
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-02-06 13:32:09 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,12 @@ function App() {
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
+      </div><div className="rating gap-1">
+        <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
       </div></>
   
   );
