23b7764d967ae78cf6eb940c8924a2f8bbd4bf0f
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-02-08 13:00:10 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <><div>
-      <button className="btn">Hello daisyUI</button>
-    </div><div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Card title!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div><div className="rating gap-1">
-        <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-        <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
-      </div></>
-  
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <h3>Home</h3>
+    },
+    {
+      path: "/setup",
+      element: <h3>Setup</h3>
+    },
+    {
+      path: "/play",
+      element: <h3>Play</h3>
+    },
+  ]);  
+
+
+  return (      
+    <div 
+      className="App p-3"
+    >
+      <RouterProvider 
+        router={router} 
+      />
+    </div>
   );
 }
-
-export default App;
