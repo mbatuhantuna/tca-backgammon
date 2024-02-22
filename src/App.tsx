7e3be59b80329273477f4ef37607b36a8639ac54
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-02-22 16:20:42 -0600

    Update Header, Title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import { Home } from './Home';
+import { Home, AppTitle } from './Home';
 import { Setup } from './Setup';
 import { Play } from './Play';
 import { GameResult, getLeaderboard } from './GameResults';
@@ -59,7 +59,7 @@ const App = () => {
   
   const [gameResults, setGameResults] = useState<GameResult[]>(dummyGameResults);
 
-  const [title, setTitle] = useState("Backgammon Companion App");
+  const [title, setTitle] = useState(AppTitle);
 
   const addNewGameResult = (result: GameResult) => setGameResults(
     [
@@ -73,16 +73,20 @@ const App = () => {
       path: "/",
       element: <Home
         leaderboardData={getLeaderboard(gameResults)}
+        setTitle={setTitle}
       />
     },
     {
       path: "/setup",
-      element: <Setup />
+      element: <Setup 
+        setTitle={setTitle}
+      />
     },
     {
       path: "/play",
       element: <Play 
           addNewGameResult={addNewGameResult}
+          setTitle={setTitle}
         />
     },
   ]);  
@@ -95,10 +99,12 @@ const App = () => {
       <div
         className='navbar bg-base-300'
       >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z" />
-</svg>
-
+        {
+          title === AppTitle &&
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
+          </svg>
+        }
         <span
           className='text-lg font-bold ml-3'
         >
