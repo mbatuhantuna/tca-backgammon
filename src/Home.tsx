fe14bfe67cb9beed7f33958660e3424c59bd7294
mbatuhantuna <mustafabatuhantuna@gmail.com>
2024-04-30 13:46:49 -0500

    Chart Button to Choose the Players

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { GeneralFacts, LeaderboardEntry } from './GameResults';
-import { FC, useEffect } from 'react';
-import PieChart from "./PieChart";
+import { FC, useEffect, useState } from 'react';
+import PieChart from "./PieChart"
 
 export const AppTitle = "Backgammon Companion App";
 
@@ -26,6 +26,7 @@ export const Home: FC<HomeProps> = ({
     , avgGameDurationsByPlayerCount
  }) => {
 
+    const [playerForChart, setPlayerForChart] = useState("");
     console.log(avgGameDurationsByPlayerCount);
     useEffect(
         () => setTitle(AppTitle)
@@ -103,6 +104,7 @@ export const Home: FC<HomeProps> = ({
                                         <th>AVG</th>
                                         <th>PLAYER</th>
                                         <th>AVG PLAYER DOUBLE</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -116,6 +118,14 @@ export const Home: FC<HomeProps> = ({
                                                 <td>{ lbe.avg.toFixed(3) }</td>
                                                 <td>{ lbe.name }</td>
                                                 <td>{ lbe.abgPlayerDoubles }</td>
+                                                <td>
+                                                    <button 
+                                                        className="btn btn-link"
+                                                        onClick={() => setPlayerForChart(lbe.name)}
+                                                    >
+                                                        Chart
+                                                    </button>
+                                                </td>
                                             </tr>
                                         ))
                                     }
@@ -193,9 +203,9 @@ className=
             Chart Explanation
         </h2>
         <h1
-            className='card-title mt-4 flex justify-center items-center h-full w-full'
+            className='card-title mt- flex justify-center items-center h-full w-full'
         >
-            Chosen Player
+               { playerForChart }
         </h1>
     </div>
 
